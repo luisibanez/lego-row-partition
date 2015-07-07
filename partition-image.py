336dbb9deba1c50ff65c2287a0e581db5142a46c
Luis Ibanez <luisi@google.com>
2015-07-07 11:56:23 -0400

    Simplified by using sets and dictionary.
    Thanks to Tony Lieuallen for guidance.

--- partition-image.py
+++ partition-image.py
@@ -11,114 +11,69 @@ def analyzeRows(image):
   length = 0
   color = -1
 
-  sum = 0
-
   for y in range(height):
     for x in range(width):
       cpixel = pixels[x, y]
       if (color != cpixel) or (x == 0):
         if length > 0:
-          sum += length
           partitionBlock(color, length)
-        color = cpixel  
+        color = cpixel
         length = 1
       else:
         length += 1
 
-  print "sum ", sum
+  if length > 0:
+    partitionBlock(color, length)
 
 
 def partitionBlock(color, length):
 
-  if color not in dist:
-    print "ADDING ", color
-    dist[color] = {
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      6: 0,
-      8: 0,
-      10: 0,
-      12: 0,
-      16: 0,
-      }    
+  piece_lengths = {1, 2, 3, 4, 6, 8, 10, 12, 16}
 
-  print length, color
- 
+  length_splits = {
+      14: 6,
+      18: 8,
+      20: 10,
+      22: 10,
+      24: 12,
+      26: 10,
+      28: 12,
+      30: 8,
+      }
+
+  if color not in dist:
+    newdict = {}
+    for l in piece_lengths:
+      newdict[l] = 0
+    dist[color] = newdict
+
   while length > 0:
-    print "current ", length
-    if length == 1:
-      dist[color][1] += 1
-      length = 0
-    elif length == 2:
-      dist[color][2] += 1
-      length = 0
-    elif length == 3:
-      dist[color][3] += 1
-      length = 0
-    elif length == 4:
-      dist[color][4] += 1
-      length = 0
-    elif length & 1:
-      dist[color][3] += 1
+    if length > 3 and length & 1:
+      dist[color][3] += 1   # 3-length pieces are special
       length -= 3
-    elif length == 6:
-      dist[color][6] += 1
+    elif length in piece_lengths:
+      dist[color][length] += 1
       length = 0
-    elif length == 8:
-      dist[color][8] += 1
-      length = 0
-    elif length == 10:
-      dist[color][10] += 1
-      length = 0
-    elif length == 12:
-      dist[color][12] += 1
-      length = 0
-    elif length == 14:
-      dist[color][6] += 1
-      dist[color][8] += 1
-      length = 0
-    elif length == 16:
-      dist[color][16] += 1
-      length = 0
-    elif length == 18:
-      dist[color][8] += 1
-      length -= 8
-    elif length == 20:
-      dist[color][10] += 1
-      length -= 10
-    elif length == 22:
-      dist[color][10] += 1
-      length -= 10
-    elif length == 24:
-      dist[color][12] += 1
-      length -= 12
-    elif length == 26:
-      dist[color][10] += 1
-      length -= 10
-    elif length == 28:
-      dist[color][12] += 1
-      length -= 12
-    elif length == 30:
-      dist[color][8] += 1
-      length -= 8
+    elif length in length_splits:
+      largest_smallest_length = length_splits[length]
+      dist[color][largest_smallest_length] += 1
+      length -= largest_smallest_length
     else:
       dist[color][16] += 1
       length -= 16
 
-  print dist[color]
 
 
 def main():
   portrait = Image.open(sys.argv[1])
   analyzeRows(portrait)
-  print dist
 
-  total = 0 
+  total = 0
   for color in dist:
+    print "color", color
+    print dist[color]
     for size in dist[color]:
-      total += dist[color][size]
+      total += dist[color][size] * size
 
   print "total pixels ", total
 
